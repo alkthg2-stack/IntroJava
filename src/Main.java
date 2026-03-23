a8cbcb54f096915c329e0b5901d00d74f86a7a9c
alkthg2-stack <alkthg2@gmail.com>
2026-03-23 20:31:06 +0100

    Exercice 6

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
 public class Main {
     static void main() {
-        System.out.println("--Exercice 5--");
+        System.out.println("--Exercice 6--");
 
-        int a = 2;
-        int b = 2;
-        int c = 2;
+        for(int i=1;i<=10;i++){
+            System.out.println("i est égal à : "+i);
+        }
 
-        if(a==b && b==c){
-            System.out.println("Les trois variables sont égales");
-        }
-        else{
-            System.out.println("Les trois variables ne sont pas identiques");
-        }
 
     }
 }
