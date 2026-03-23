6217e0c69ed45d06afbd01bea43b6aaceb06138e
alkthg2-stack <alkthg2@gmail.com>
2026-03-23 19:49:30 +0100

    Exercice 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     static void main() {
         System.out.println("--Exercice 1--");
+
+        System.out.println("Variable Char");
+        char a ='K';
+        System.out.println(a);
+
+        System.out.println("Variable int");
+        int b = 1;
+        System.out.println(b);
+
+        System.out.println("Variable long");
+        long c = 999999999;
+        System.out.println(c);
+
+        System.out.println("Variable float");
+        float d = 0.9F;
+        System.out.println(d);
+
+        System.out.println("Variable double");
+        double e = 1231.22312;
+        System.out.println(e);
+
+        System.out.println("Variable boolean");
+        boolean f= true;
+        System.out.println(f);
+
+        System.out.println("Variable string");
+        String g = "Ketadown";
+        System.out.println(g);
     }
 }
