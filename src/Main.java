662dda6cd2791f7a6744fcf68c9d6043d7cf8b35
alkthg2-stack <alkthg2@gmail.com>
2026-03-23 20:18:43 +0100

    Exercice 4

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,94 @@
 public class Main {
     static void main() {
-        System.out.println("--Exercice 3--");
+        System.out.println("--Exercice 4--");
 
         int a = 3;
-        int b = 3;
+        int b = 11;
+        int c = 5;
+        int d = 8;
+        int i = 0;
 
         if(a==b){
-            System.out.println("a=b");
+             boolean e=true;
+             i++;
+            System.out.println("La condition "+i+" retournera :");
+            System.out.println(e);
         }
         else{
-            System.out.println("a et b sont différents");
+            boolean e=false;
+            i++;
+            System.out.println("La condition "+i+" retournera :");
+            System.out.println(e);
         }
+
+        if(b == a+d){
+            boolean e=true;
+            i++;
+            System.out.println("La condition "+i+" retournera :");
+            System.out.println(e);
+        }
+        else{
+            boolean e=false;
+            i++;
+            System.out.println("La condition "+i+" retournera :");
+            System.out.println(e);
+        }
+
+        if((b == a+d) && (a == b)){
+            boolean e=true;
+            i++;
+            System.out.println("La condition "+i+" retournera :");
+            System.out.println(e);
+        }
+        else{
+            boolean e=false;
+            i++;
+            System.out.println("La condition "+i+" retournera :");
+            System.out.println(e);
+        }
+
+        if((b == a) || (a==a)){
+            boolean e=true;
+            i++;
+            System.out.println("La condition "+i+" retournera :");
+            System.out.println(e);
+        }
+        else{
+            boolean e=false;
+            i++;
+            System.out.println("La condition "+i+" retournera :");
+            System.out.println(e);
+        }
+
+        if((c == 5) && (false)){
+            boolean e=true;
+            i++;
+            System.out.println("La condition "+i+" retournera :");
+            System.out.println(e);
+        }
+        else{
+            boolean e=false;
+            i++;
+            System.out.println("La condition "+i+" retournera :");
+            System.out.println(e);
+        }
+
+        if(((4 == 4) && (4 ==5)) || (a == a)){
+            boolean e=true;
+            i++;
+            System.out.println("La condition "+i+" retournera :");
+            System.out.println(e);
+        }
+        else{
+            boolean e=false;
+            i++;
+            System.out.println("La condition "+i+" retournera :");
+            System.out.println(e);
+        }
+
+
+
+
+
     }
 }
